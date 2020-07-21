ac43a248034a509789d687f424422b186c8f3ce2
Francesco Murdaca <fmurdaca@redhat.com>
2020-07-21 18:40:37 +0200

    Add metrics
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

---
+++ thoth/advise_reporter/metrics.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# thoth-adviser-reporter
+# Copyright(C) 2020 Francesco Murdaca
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+"""This is Thoth adviser-reporter consumer metrics."""
+
+
+from thoth.advise_reporter import __service_version__
+
+from prometheus_client import CollectorRegistry, Gauge, Counter
+
+
+# A registry for the consumer metrics...
+prometheus_registry = CollectorRegistry()
+
+# add the application version info metric
+_API_GAUGE_METRIC = Gauge(
+    "advise_reporter_consumer_info",
+    "Adviser reporter Version Info",
+    labelnames=["version"],
+    registry=prometheus_registry,
+)
+_API_GAUGE_METRIC.labels(version=__service_version__).inc()
+
+
+# Advise justifications
+
+METRIC_ADVISE_TYPE = Gauge(
+    "thoth_advise_message_number",
+    "Number of thamos advise provided per message.",
+    ["advise_message", "thoth_environment"],
+)

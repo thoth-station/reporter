39f10a4438bebfa08fc965babb2edd96fb0bca70
Francesco Murdaca <fmurdaca@redhat.com>
2020-07-24 18:38:35 +0200

    Add env variables and adjust logic
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/advise_reporter/advise_reporter.py
+++ thoth/advise_reporter/advise_reporter.py
@@ -34,13 +34,16 @@ DEPLOYMENT_NAME = os.environ["THOTH_DEPLOYMENT_NAME"]
 ENVIRONMENT = os.environ["THOTH_ENVIRONMENT"]
 CEPH_BUCKET_PREFIX = os.environ["THOTH_CEPH_BUCKET_PREFIX"]
 PUBLIC_CEPH_BUCKET = os.environ["THOTH_PUBLIC_CEPH_BUCKET"]
+LIMIT_RESULTS = bool(int(os.getenv("THOTH_LIMIT_RESULTS", 1)))
+MAX_IDS = int(os.getenv("THOTH_MAX_IDS"))
+IS_STORING = bool(int(os.getenv("THOTH_IS_STORING", 0)))
+
 
 
 def produce_adviser_reports_justifications_dataframe(adviser_version: str) -> pd.DataFrame:
     """Produce adviser reports justifications dataframe."""
-    adviser_files = Adviser.aggregate_adviser_results(is_local=False, max_ids=50, limit_results=True)
+    adviser_files = Adviser.aggregate_adviser_results(is_local=False, limit_results=LIMIT_RESULTS, max_ids=MAX_IDS)
     adviser_dataframe = Adviser.create_adviser_dataframe(adviser_version=adviser_version, adviser_files=adviser_files)
-
     final_dataframe = Adviser.create_summary_dataframe(adviser_dataframe=adviser_dataframe)
 
     return final_dataframe
@@ -52,30 +55,33 @@ def parse_adviser_dataframe(
     """Parse final dataframe to produce messages depending on the justification type."""
     justification_dataframe = final_dataframe[final_dataframe["type"] == justification_type]
 
-    adviser_heatmap_df = Adviser.create_adviser_results_dataframe_heatmap(
-        adviser_type_dataframe=justification_dataframe, number_days=1
-    )
-    last_date = [column for column in adviser_heatmap_df.columns][-1]
-
     advise_justifications: List[Dict[str, Any]] = []
 
-    if last_date > date_filter:
-        subset_adviser_results = adviser_heatmap_df[[last_date]]
-
-        for index, row in subset_adviser_results[[last_date]].iterrows():
-            message = index
-            if message not in [m["message"] for m in advise_justifications] and row[last_date] > 0:
-                advise_justifications.append(
-                    {"date": last_date.strftime("%Y-%m-%d"), "message": message, "count": row[last_date]}
-                )
-
-        advise_justification_df = pd.DataFrame(advise_justifications)
-
-        store_to_ceph(
-            advise_justification_df=advise_justification_df,
-            date_filter=date_filter,
-            justification_type=justification_type,
+    if not justification_dataframe.empty:
+        adviser_heatmap_df = Adviser.create_adviser_results_dataframe_heatmap(
+            adviser_type_dataframe=justification_dataframe, number_days=1
         )
+        last_date = [column for column in adviser_heatmap_df.columns][-1]
+
+        if not adviser_heatmap_df.empty and last_date > date_filter:
+            subset_adviser_results = adviser_heatmap_df[[last_date]]
+
+            for index, row in subset_adviser_results[[last_date]].iterrows():
+                message = index
+                if message not in [m["message"] for m in advise_justifications] and row[last_date] > 0:
+                    advise_justifications.append(
+                        {"date": last_date.strftime("%Y-%m-%d"), "message": message, "count": row[last_date]}
+                    )
+
+            advise_justification_df = pd.DataFrame(advise_justifications)
+
+            if IS_STORING:
+                _LOGGER("Storing to Ceph...")
+                store_to_ceph(
+                    advise_justification_df=advise_justification_df,
+                    date_filter=date_filter,
+                    justification_type=justification_type,
+                )
 
     return advise_justifications
 

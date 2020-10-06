567a6e8a004e1de0b3ee18ad55875a6d9efdf69f
Francesco Murdaca <fmurdaca@redhat.com>
2020-10-06 09:54:33 +0200

    Refactor logic and use thoth-report-processing methods
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- producer.py
+++ producer.py
@@ -21,58 +21,95 @@ import logging
 import os
 import datetime
 
+import pandas as pd
+
 from thoth.messaging import MessageBase, AdviseJustificationMessage
-from thoth.advise_reporter.advise_reporter import produce_adviser_reports_justifications_dataframe
-from thoth.advise_reporter.advise_reporter import parse_adviser_dataframe
+from thoth.report_processing.components.adviser import Adviser
+from thoth.advise_reporter.advise_reporter import parse_summary_dataframe, save_results_to_ceph
 from thoth.advise_reporter import __service_version__
+from thoth.common import init_logging
+
+_LOGGER = logging.getLogger("thoth.advise_reporter")
+_LOGGER.info("Thoth advise reporter producer v%s", __service_version__)
 
 app = MessageBase().app
 
-ADVISER_VERSION = os.getenv("ADVISER_VERSION")
-
+ADVISER_VERSION = os.environ["ADVISER_VERSION"]
+ONLY_STORE = bool(int(os.getenv("THOTH_ADVISE_REPORTER_ONLY_STORE", 0)))
 COMPONENT_NAME = "advise_reporter"
-
-_LOGGER = logging.getLogger(__name__)
-
+EVALUATION_METRICS_DAYS = int(os.getenv("THOTH_EVALUATION_METRICS_NUMBER_DAYS", 1))
+LIMIT_RESULTS = bool(int(os.getenv("THOTH_LIMIT_RESULTS", 0)))
+MAX_IDS = int(os.getenv("THOTH_MAX_IDS", 100))
+_LOGGER.info(f"THOTH_EVALUATION_METRICS_NUMBER_DAYS set to {EVALUATION_METRICS_DAYS}.")
 
 @app.command()
 async def main():
     """Run advise-reporter to produce message."""
+    init_logging()
     _advise_justification = AdviseJustificationMessage()
 
-    date = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    adviser_files = Adviser.aggregate_adviser_results(
+        limit_results=LIMIT_RESULTS,
+        max_ids=MAX_IDS,
+    )
+    adviser_dataframe = Adviser.create_adviser_dataframe(adviser_version=ADVISER_VERSION, adviser_files=adviser_files)
 
-    final_dataframe = produce_adviser_reports_justifications_dataframe(adviser_version=ADVISER_VERSION)
+    adviser_summary_dataframe = Adviser.create_summary_dataframe(adviser_dataframe=adviser_dataframe)
 
-    for justification_type in ["INFO", "ERROR", "WARNING"]:
-        advise_justifications = parse_adviser_dataframe(
-            final_dataframe=final_dataframe, date_filter=date, justification_type=justification_type
-        )
+    for i in range(0, EVALUATION_METRICS_DAYS):
+        date = datetime.datetime.utcnow() - datetime.timedelta(days=i)
+        _LOGGER.info(f"Date considered: {date.strftime('%Y-%m-%d')}")
+
+        advise_justifications: List[Dict[str, Any]] = []
+
+        for justification_type in ["INFO", "ERROR", "WARNING"]:
+
+            advise_justifications = parse_summary_dataframe(
+                advise_justifications=advise_justifications,
+                summary_dataframe=adviser_summary_dataframe,
+                date_filter=date,
+                justification_type=justification_type,
+            )
 
         if not advise_justifications:
-            _LOGGER.info(f"No adviser justifications of type {justification_type} found in date: {date}")
+            _LOGGER.info(f"No adviser justifications found in date: {date.strftime('%Y-%m-%d')}")
 
-        for advise_justification in advise_justifications:
-            message = advise_justification["message"]
-            count = advise_justification["count"]
-            try:
-                await _advise_justification.publish_to_topic(
-                    _advise_justification.MessageContents(
-                        message=message,
-                        count=count,
-                        justification_type=justification_type,
-                        component_name=COMPONENT_NAME,
-                        service_version=__service_version__,
-                    )
+            if EVALUATION_METRICS_DAYS == 1:
+                return 
+
+        advise_justification_df = pd.DataFrame(advise_justifications)
+
+        save_results_to_ceph(
+            advise_justification_df=advise_justification_df,
+            date_filter=date
+        )
+
+    if ONLY_STORE or EVALUATION_METRICS_DAYS > 1:
+        return
+
+    for advise_justification in advise_justifications:
+        message = advise_justification["message"]
+        count = advise_justification["count"]
+        justification_type = advise_justification["type"]
+
+        try:
+            await _advise_justification.publish_to_topic(
+                _advise_justification.MessageContents(
+                    message=message,
+                    count=int(count),
+                    justification_type=justification_type,
+                    component_name=COMPONENT_NAME,
+                    service_version=__service_version__,
                 )
-                _LOGGER.debug(
-                    "Adviser justification message:\n%r\nJustification type:\n%r\nCount:\n%r\n",
-                    message,
-                    justification_type,
-                    count,
-                )
-            except Exception as identifier:
-                _LOGGER.exception("Failed to publish with the following error message: %r", identifier)
+            )
+            _LOGGER.debug(
+                "Adviser justification message:\n%r\nJustification type:\n%r\nCount:\n%r\n",
+                message,
+                justification_type,
+                count,
+            )
+        except Exception as identifier:
+            _LOGGER.exception("Failed to publish with the following error message: %r", identifier)
 
 
 if __name__ == "__main__":
--- thoth/advise_reporter/advise_reporter.py
+++ thoth/advise_reporter/advise_reporter.py
@@ -25,8 +25,6 @@ import pandas as pd
 from typing import Dict, Any, List
 
 from thoth.report_processing.components import Adviser
-from thoth.messaging import MessageBase
-from thoth.advise_reporter import metrics
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,9 +32,8 @@ DEPLOYMENT_NAME = os.environ["THOTH_DEPLOYMENT_NAME"]
 ENVIRONMENT = os.environ["THOTH_ENVIRONMENT"]
 CEPH_BUCKET_PREFIX = os.environ["THOTH_CEPH_BUCKET_PREFIX"]
 PUBLIC_CEPH_BUCKET = os.environ["THOTH_PUBLIC_CEPH_BUCKET"]
-LIMIT_RESULTS = bool(int(os.getenv("THOTH_LIMIT_RESULTS", 0)))
-MAX_IDS = int(os.getenv("THOTH_MAX_IDS", 100))
-IS_STORING = bool(int(os.getenv("THOTH_IS_STORING", 0)))
+
+IS_STORING = bool(int(os.getenv("THOTH_IS_STORING", 1)))
 
 
 def produce_adviser_reports_justifications_dataframe(adviser_version: str) -> pd.DataFrame:
@@ -48,13 +45,17 @@ def produce_adviser_reports_justifications_dataframe(adviser_version: str) -> pd
     return final_dataframe
 
 
-def parse_adviser_dataframe(
-    final_dataframe: pd.DataFrame, date_filter: datetime.datetime, justification_type: str
+def parse_summary_dataframe(
+    advise_justifications: List[Dict[str, Any]],
+    summary_dataframe: pd.DataFrame,
+    date_filter: datetime.datetime,
+    justification_type: str,
 ) -> List[Dict[str, Any]]:
-    """Parse final dataframe to produce messages depending on the justification type."""
-    justification_dataframe = final_dataframe[final_dataframe["type"] == justification_type]
+    """Parse final dataframe to produce messages depending on the justification type.
 
-    advise_justifications: List[Dict[str, Any]] = []
+    :param summary_dataframe: DataFrame as returned by `Adviser.create_summary_dataframe` method.
+    """
+    justification_dataframe = summary_dataframe[summary_dataframe["type"] == justification_type]
 
     if not justification_dataframe.empty:
         adviser_heatmap_df = Adviser.create_adviser_results_dataframe_heatmap(
@@ -83,28 +84,28 @@ def parse_adviser_dataframe(
                 message = index
                 if message not in [m["message"] for m in advise_justifications] and row[selected_date] > 0:
                     advise_justifications.append(
-                        {"date": selected_date.strftime("%Y-%m-%d"), "message": message, "count": row[selected_date]}
+                        {
+                            "date": selected_date.strftime("%Y-%m-%d"),
+                            "message": message,
+                            "count": row[selected_date],
+                            "type": justification_type,
+                        }
                     )
-
-            advise_justification_df = pd.DataFrame(advise_justifications)
-
-            if advise_justification_df.empty:
-                return advise_justifications
-
-            if IS_STORING:
-                _LOGGER.info("Storing to Ceph...")
-                store_to_ceph(
-                    advise_justification_df=advise_justification_df,
-                    date_filter=date_filter,
-                    justification_type=justification_type,
-                )
 
     return advise_justifications
 
 
-def store_to_ceph(
-    advise_justification_df: pd.DataFrame, date_filter: datetime.datetime, justification_type: str
-) -> None:
+def save_results_to_ceph(advise_justification_df: pd.DataFrame, date_filter: datetime.datetime):
+    """Save results on Ceph."""
+    if advise_justification_df.empty:
+        return advise_justification_df
+
+    if IS_STORING:
+        _LOGGER.info("Storing to Ceph...")
+        _store_to_ceph(advise_justification_df=advise_justification_df, date_filter=date_filter)
+
+
+def _store_to_ceph(advise_justification_df: pd.DataFrame, date_filter: datetime.datetime) -> None:
     """Store results to Ceph for visualization."""
     ceph_sli = Adviser.connect_to_ceph(
         ceph_bucket_prefix=CEPH_BUCKET_PREFIX, processed_data_name="thoth-sli-metrics", environment=ENVIRONMENT
@@ -117,18 +118,22 @@ def store_to_ceph(
         bucket=PUBLIC_CEPH_BUCKET,
     )
 
-    result_class = f"adviser-justifications-{justification_type}"
+    result_class = f"adviser-justifications"
     ceph_path = f"{result_class}/{result_class}-{date_filter.strftime('%Y-%m-%d')}.csv"
 
-    csv: str = advise_justification_df.to_csv(header=False)
+    _LOGGER.info(f"Results to be stored on Ceph...{advise_justification_df}")
+
+    csv: str = advise_justification_df.to_csv(header=False, sep ='`', index=False)
 
     try:
         Adviser.store_csv_from_dataframe(
-            csv_from_df=csv, ceph_sli=ceph_sli, file_name=result_class, ceph_path=ceph_path,
+            csv_from_df=csv, ceph_sli=ceph_sli, file_name=result_class, ceph_path=ceph_path
         )
     except Exception as e_ceph:
         _LOGGER.exception(f"Could not store metrics on Thoth bucket on Ceph...{e_ceph}")
         pass
+
+    _LOGGER.info(f"Successfully stored in Thoth bucket on Ceph...{ceph_path}")
 
     try:
         Adviser.store_csv_from_dataframe(
@@ -138,21 +143,4 @@ def store_to_ceph(
         _LOGGER.exception(f"Could not store metrics on Public bucket on Ceph...{e_ceph}")
         pass
 
-
-@metrics.exceptions.count_exceptions()
-@metrics.in_progress.track_inprogress()
-def expose_metrics(advise_justification: MessageBase):
-    """Retrieve adviser reports justifications."""
-    metrics.advise_justification_type_number.labels(
-        advise_message=advise_justification.message,
-        justification_type=advise_justification.justification_type,
-        thoth_environment=DEPLOYMENT_NAME,
-    ).inc(advise_justification.count)
-    _LOGGER.info(
-        "advise_justification_type_number(%r, %r)=%r",
-        advise_justification.message,
-        advise_justification.justification_type,
-        advise_justification.count,
-    )
-
-    metrics.success.inc()
+    _LOGGER.info(f"Successfully stored in Public bucket on Ceph...{ceph_path}")

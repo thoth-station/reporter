145db173fc4d17207b2f0a5e453374fc34168c34
Francesco Murdaca <fmurdaca@redhat.com>
2021-04-29 14:26:28 +0200

    Produce requests vs reports analysis (#200)
    * rename to thoth-reporter
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * introduce analysis requests - reports
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * update README
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * remove-reviewers
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * update env variables
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Make pre-commit happy
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * make mypy happy
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- producer.py
+++ producer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# adviser-reporter
-# Copyright(C) 2020 Francesco Murdaca
+# thoth-reporter
+# Copyright(C) 2020, 2021 Francesco Murdaca
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,13 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""This is run periodically to provide metrics regarding Advise provided to Thoth Users."""
+"""This is run periodically to provide metrics regarding Thoth services to Thoth contributors."""
 
 import logging
 import os
 import datetime
-
-import pandas as pd
 
 from typing import Dict, Any, List
 
@@ -29,30 +27,30 @@ import thoth.messaging.producer as producer
 from thoth.messaging import advise_justification_message
 from thoth.messaging.advise_justification import MessageContents as AdviseJustificationContents
 
-from thoth.report_processing.components.adviser import Adviser
-from thoth.advise_reporter.utils import save_results_to_ceph
-from thoth.advise_reporter.processed_results import retrieve_processed_justifications_dataframe
-from thoth.advise_reporter.processed_results import retrieve_processed_statistics_dataframe
-from thoth.advise_reporter.processed_results import retrieve_processed_inputs_info_dataframe
+from thoth.reporter.processing import evaluate_requests_statistics, explore_adviser_files
 
-from thoth.advise_reporter import __service_version__
+from thoth.reporter import __service_version__
 
 from thoth.storages import GraphDatabase
+from thoth.storages.advisers import AdvisersResultsStore
+from thoth.storages.analyses import AnalysisResultsStore
+from thoth.storages.provenance import ProvenanceResultsStore
+
 
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
-_LOGGER = logging.getLogger("thoth.advise_reporter")
-_LOGGER.info("Thoth advise reporter producer v%s", __service_version__)
+_LOGGER = logging.getLogger("thoth.thoth_reporter")
+_LOGGER.info("Thoth thoth reporter producer v%s", __service_version__)
 
-COMPONENT_NAME = "advise_reporter"
+COMPONENT_NAME = "thoth_reporter"
 
 _THOTH_DEPLOYMENT_NAME = os.getenv("THOTH_DEPLOYMENT_NAME")
 
-_SEND_MESSAGES = bool(int(os.getenv("THOTH_ADVISE_REPORTER_SEND_KAFKA_MESSAGES", 0)))
-_STORE_ON_CEPH = bool(int(os.getenv("THOTH_ADVISE_REPORTER_STORE_ON_CEPH", 1)))
-_STORE_ON_PUBLIC_CEPH = bool(int(os.getenv("THOTH_ADVISE_REPORTER_STORE_ON_PUBLIC_CEPH", 0)))
+_SEND_MESSAGES = bool(int(os.getenv("THOTH_REPORTER_SEND_KAFKA_MESSAGES", 0)))
+STORE_ON_CEPH = bool(int(os.getenv("THOTH_REPORTER_STORE_ON_CEPH", 1)))
+STORE_ON_PUBLIC_CEPH = bool(int(os.getenv("THOTH_REPORTER_STORE_ON_PUBLIC_CEPH", 0)))
 
-_SEND_METRICS = bool(int(os.getenv("THOTH_ADVISE_REPORTER_SEND_METRICS", 1)))
+_SEND_METRICS = bool(int(os.getenv("THOTH_REPORTER_SEND_METRICS", 1)))
 
 if _SEND_MESSAGES:
     p = producer.create_producer()
@@ -71,13 +69,29 @@ MAX_IDS = int(os.getenv("THOTH_MAX_IDS", 100))
 
 prometheus_registry = CollectorRegistry()
 
-thoth_adviser_reporter_info = Gauge(
-    "advise_reporter_info",
-    "Thoth Adviser Reporter information",
+# Define metrics
+
+thoth_reporter_info = Gauge(
+    "thoth_reporter_info",
+    "Thoth Reporter information",
     ["version"],
     registry=prometheus_registry,
 )
-thoth_adviser_reporter_info.labels(__service_version__).inc()
+thoth_reporter_info.labels(__service_version__).inc()
+
+thoth_reporter_requests_gauge = Gauge(
+    "thoth_reporter_requests_gauge",
+    "Thoth Reporter requests created per component",
+    ["component"],
+    registry=prometheus_registry,
+)
+
+thoth_reporter_reports_gauge = Gauge(
+    "thoth_reporter_reports_gauge",
+    "Thoth Reporter reports created per component",
+    ["component"],
+    registry=prometheus_registry,
+)
 
 if _THOTH_METRICS_PUSHGATEWAY_URL:
     _METRIC_DATABASE_SCHEMA_SCRIPT = Gauge(
@@ -88,25 +102,39 @@ if _THOTH_METRICS_PUSHGATEWAY_URL:
     )
 
     _METRIC_DATABASE_SCHEMA_SCRIPT.labels(
-        "advise-reporter", GraphDatabase().get_script_alembic_version_head(), _THOTH_DEPLOYMENT_NAME
+        "thoth-reporter", GraphDatabase().get_script_alembic_version_head(), _THOTH_DEPLOYMENT_NAME
     ).inc()
+
+ADVISER_STORE = AdvisersResultsStore()
+ADVISER_STORE.connect()
+
+PACKAGE_EXTRACT_STORE = AnalysisResultsStore()
+PACKAGE_EXTRACT_STORE.connect()
+
+PROVENANCE_STORE = ProvenanceResultsStore()
+PROVENANCE_STORE.connect()
+
+RESULTS_STORES = {
+    "adviser": ADVISER_STORE,
+    "package-extract": PACKAGE_EXTRACT_STORE,
+    "provenance-checker": PROVENANCE_STORE,
+}
 
 TODAY = datetime.date.today()
 
-START_DATE = os.getenv("ADVISE_REPORTER_START_DATE", str(TODAY))
-END_DATE = os.getenv("ADVISE_REPORTER_END_DATE", str(TODAY))
+START_DATE = os.getenv("THOTH_REPORTER_START_DATE", str(TODAY))
+END_DATE = os.getenv("THOTH_REPORTER_END_DATE", str(TODAY))
 
 
 def main():
-    """Run advise-reporter to produce message."""
+    """Run thoth-reporter to provide information on status of services to Thoth contributors."""
     if not _SEND_MESSAGES:
-        _LOGGER.info("No messages are sent. THOTH_ADVISE_REPORTER_SEND_KAFKA_MESSAGES is set to 0")
+        _LOGGER.info("No messages are sent. THOTH_REPORTER_SEND_KAFKA_MESSAGES is set to 0")
 
-    total_justifications: List[Dict[str, Any]] = []
     try:
         datetime.datetime.strptime(START_DATE, "%Y-%m-%d")
     except ValueError as err:
-        _LOGGER.error(f"ADVISE_REPORTER_START_DATE uses incorrect format: {err}")
+        _LOGGER.error(f"THOTH_REPORTER_START_DATE uses incorrect format: {err}")
 
     s_date = START_DATE.split("-")
     start_date = datetime.date(year=int(s_date[0]), month=int(s_date[1]), day=int(s_date[2]))
@@ -114,7 +142,7 @@ def main():
     try:
         datetime.datetime.strptime(END_DATE, "%Y-%m-%d")
     except ValueError as err:
-        _LOGGER.error(f"ADVISE_REPORTER_END_DATE uses incorrect format: {err}")
+        _LOGGER.error(f"THOTH_REPORTER_END_DATE uses incorrect format: {err}")
 
     e_date = END_DATE.split("-")
     end_date = datetime.date(year=int(e_date[0]), month=int(e_date[1]), day=int(e_date[2]))
@@ -151,6 +179,8 @@ def main():
     _LOGGER.info(f"Initial start date: {start_date}")
     _LOGGER.info(f"Initial end date (excluded): {end_date}")
 
+    total_justifications: List[Dict[str, Any]] = []
+
     current_initial_date = start_date
 
     while current_initial_date < end_date:
@@ -160,84 +190,29 @@ def main():
         _LOGGER.info(f"Analyzing for start date: {current_initial_date}")
         _LOGGER.info(f"Analyzing for end date (excluded): {current_end_date}")
 
-        daily_processed_daframes: List[pd.DataFrame] = {}
-
-        adviser_files = Adviser.aggregate_adviser_results(start_date=current_initial_date, end_date=current_end_date)
-
-        if not adviser_files:
-            _LOGGER.info("No adviser files identifed!")
-            current_initial_date += delta
-            continue
-
-        dataframes = Adviser.create_adviser_dataframes(adviser_files=adviser_files)
-
-        daily_justifications = retrieve_processed_justifications_dataframe(
-            date_=current_initial_date, dataframes=dataframes
+        stats = evaluate_requests_statistics(
+            current_initial_date=current_initial_date,
+            current_end_date=current_end_date,
+            results_store=RESULTS_STORES,
+            store_on_ceph=STORE_ON_CEPH,
+            store_on_public_bucket=STORE_ON_PUBLIC_CEPH,
         )
-        daily_processed_daframes["adviser_justifications"] = pd.DataFrame(daily_justifications)
 
-        if not daily_processed_daframes["adviser_justifications"].empty and not _STORE_ON_CEPH:
-            _LOGGER.info(
-                "Adviser justifications:"
-                f'\n{daily_processed_daframes["adviser_justifications"].to_csv(header=False, sep="`", index=False)}'
-            )
+        # Assign metrics for pushgateway
+        for stats_analysis in stats:
 
-        daily_statistics = retrieve_processed_statistics_dataframe(date_=current_initial_date, dataframes=dataframes)
-        daily_processed_daframes["adviser_statistics"] = pd.DataFrame(daily_statistics)
+            thoth_reporter_requests_gauge.labels(stats_analysis["component"]).set(stats_analysis["requests"])
 
-        if not daily_processed_daframes["adviser_statistics"].empty and not _STORE_ON_CEPH:
-            _LOGGER.info(
-                "Adviser statistics success rate:"
-                f'\n{daily_processed_daframes["adviser_statistics"].to_csv(header=False, sep="`", index=False)}'
-            )
+            thoth_reporter_reports_gauge.labels(stats_analysis["component"]).set(stats_analysis["documents"])
 
-        daily_inputs_info = retrieve_processed_inputs_info_dataframe(date_=start_date, dataframes=dataframes)
-        daily_processed_daframes["adviser_integration_info"] = pd.DataFrame(daily_inputs_info["integration_info"])
-        daily_processed_daframes["adviser_recommendation_info"] = pd.DataFrame(daily_inputs_info["recommendation_info"])
-        daily_processed_daframes["adviser_solver_info"] = pd.DataFrame(daily_inputs_info["solver_info"])
-        daily_processed_daframes["adviser_base_image_info"] = pd.DataFrame(daily_inputs_info["base_image_info"])
-        daily_processed_daframes["adviser_hardware_info"] = pd.DataFrame(daily_inputs_info["hardware_info"])
+        explore_adviser_files(
+            current_initial_date=current_initial_date,
+            current_end_date=current_end_date,
+            total_justifications=total_justifications,
+            store_on_ceph=STORE_ON_CEPH,
+            store_on_public_bucket=STORE_ON_PUBLIC_CEPH,
+        )
 
-        if not daily_processed_daframes["adviser_integration_info"].empty and not _STORE_ON_CEPH:
-            _LOGGER.info(
-                "Adviser integration info stats:"
-                f'\n{daily_processed_daframes["adviser_integration_info"].to_csv(header=False, sep="`", index=False)}'
-            )
-
-        if not daily_processed_daframes["adviser_recommendation_info"].empty and not _STORE_ON_CEPH:
-            _LOGGER.info(
-                "Adviser recomendation info stats:"
-                f'\n{daily_processed_daframes["adviser_recommendation_info"].to_csv(header=False, sep="`", index=False)}'
-            )
-
-        if not daily_processed_daframes["adviser_solver_info"].empty and not _STORE_ON_CEPH:
-            _LOGGER.info(
-                "Adviser solver info stats:"
-                f'\n{daily_processed_daframes["adviser_solver_info"].to_csv(header=False, sep="`", index=False)}'
-            )
-
-        if not daily_processed_daframes["adviser_base_image_info"].empty and not _STORE_ON_CEPH:
-            _LOGGER.info(
-                "Adviser base image info stats:"
-                f'\n{daily_processed_daframes["adviser_base_image_info"].to_csv(header=False, sep="`", index=False)}'
-            )
-
-        if not daily_processed_daframes["adviser_hardware_info"].empty and not _STORE_ON_CEPH:
-            _LOGGER.info(
-                "Adviser hardware info stats:"
-                f'\n{daily_processed_daframes["adviser_hardware_info"].to_csv(header=False, sep="`", index=False)}'
-            )
-
-        if _STORE_ON_CEPH:
-            for result_class, processed_df in daily_processed_daframes.items():
-                save_results_to_ceph(
-                    processed_df=processed_df,
-                    result_class=result_class,
-                    date_filter=current_initial_date,
-                    store_to_public_ceph=_STORE_ON_PUBLIC_CEPH,
-                )
-
-        total_justifications += daily_justifications
         current_initial_date += delta
 
     if _SEND_METRICS:
